594b53c0949150cb70cc870a57ab422fa0bc4e0c
Victor Brønsholm Nielsen <72500348+TheProbPro@users.noreply.github.com>
2025-11-19 14:37:19 +0100

    removed homing on motor

--- AAN+RAN_OIACILC.py
+++ AAN+RAN_OIACILC.py
@@ -569,8 +569,6 @@ if __name__ == "__main__":
     
     # 等待并初始化电机位置
     time.sleep(1.0)
-    motor.sendMotorCommand(motor.motor_ids[0], motor_center)
-    time.sleep(1.0)
     
     # 启动EMG传感器
     emg.start()
